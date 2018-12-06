6568e0653d637eae3a2a5ef9771e9afeaa34ccf6
Dil M <miralimov@gmail.com>
2018-12-06 15:09:15 -0500

    Add files via upload

---
+++ tashkent.js
@@ -0,0 +1,19 @@
+// function myFunc(){
+// 	var x = document.getElementById("header").getAttribute("class");
+// 	document.getElementById("header").height="50px";
+// }
+
+
+// window.onscroll = changePos;
+
+// function changePos() {
+//     var header = document.getElementById("header");
+//     if (window.pageYOffset = 50) {
+//         header.style.position = "fixed";
+//         header.style.height = pageYOffset + "px";
+//         header.style.nav.fontSize = 50;
+//     } else {
+//         header.style.position = "";
+//         header.style.height = "100px";
+//     }
+// }
